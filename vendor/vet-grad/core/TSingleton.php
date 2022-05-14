72d457b2362a3971192813c3febf0146cba0afdf
Лебедева Наталья <NataRaposa@yandex.ru>
2022-05-14 21:34:11 +0200

    TSingleton.php: класс, реализующий паттерн singleton

---
+++ vendor/vet-grad/core/TSingleton.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace vetgrad;
+
+trait TSingleton
+{
+	private static $instance;
+
+	public static function instance()
+	{
+		if (self::$instance === null) {
+			self::$instance = new self;
+		}
+		return self::$instance;
+	}
+}
