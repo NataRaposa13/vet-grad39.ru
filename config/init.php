acc02f587918287f96dd07964ada5137a3bb1f84
Лебедева Наталья <NataRaposa@yandex.ru>
2022-05-14 20:59:16 +0200

    init.php: основные константы с путями

---
+++ config/init.php
@@ -0,0 +1,25 @@
+<?php /** @noinspection ALL */
+
+define("DEBUG", 1); //режим разработчика
+//define("DEBUG", 0); //режим продакшена
+
+define("ROOT", dirname(__DIR__));
+define("WWW", ROOT . '/public');
+define("APP", ROOT . '/app');
+define("CORE", ROOT . '/vendor/eshop/core');
+define("LIBS", ROOT . '/vendor/eshop/core/libs');
+define("CACHE", ROOT . '/tmp/cache');
+define("CONF", ROOT . '/config');
+define("LAYOUT", 'default');
+
+//http://vet-grad39.ru/public/index.php
+$app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
+//http://vet-grad39.ru/public/
+$app_path = preg_replace("#[^/]+$#", '', $app_path);
+//http://vet-grad39.ru
+$app_path = str_replace('/public/', '', $app_path);
+
+define("PATH", $app_path);
+define("ADMIN", PATH . '/admin');
+
+require_once ROOT . "/vendor/autoload.php";
