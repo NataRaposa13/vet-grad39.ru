16f6ed240477047769f2eaee010cef3d1c07de16
Лебедева Наталья <NataRaposa@yandex.ru>
2022-05-14 21:35:17 +0200

    App.php: класc контейнер свойств

---
+++ vendor/vet-grad/core/App.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace vetgrad;
+
+class App
+{
+	public static $app;
+
+	public function __construct()
+	{
+		$query = trim($_SERVER['QUERY_STRING'], '/');
+		session_start();
+		self::$app = Registry::instance();
+		$this->getParams();
+	}
+
+	protected function getParams()
+	{
+		$params = require_once CONF . '/params.php';
+		if (!empty($params)) {
+			foreach ($params as $key => $value) {
+				self::$app->setProperty($key, $value);
+			}
+		}
+	}
+}
