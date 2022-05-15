cf942f5f00032697ce5e1d8640818ac4af4e1df2
Лебедева Наталья <NataRaposa@yandex.ru>
2022-05-15 17:21:23 +0200

    Db.php: класс с подключением в базе данных

---
+++ vendor/vet-grad/core/Db.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace vetgrad;
+
+use RedBeanPHP\R as R;
+
+class Db
+{
+	use TSingleton;
+
+	protected function __construct()
+	{
+		$db = require_once CONF . '/config_db.php';
+		R::setup($db['dsn'], $db['user'], $db['pass']);
+		if (!R::testConnection()) {
+			throw new \Exception('Нет соединения с БД', 500);
+		}
+		R::freeze(true);
+		if (DEBUG) {
+			R::debug(true, 1);
+		}
+	}
+}
