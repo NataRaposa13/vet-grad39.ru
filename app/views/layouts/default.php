32035a3bbaebda3e567ede5f0e43e000ac21dd5d
Pavel Shorokhov <web.shorohov@yandex.ru>
2022-05-15 21:41:16 +0200

    Подготовил структуру файлов

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -5,10 +5,18 @@
 	<meta name="viewport"
 		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bellota+Text:wght@300;400;700&family=Comfortaa:wght@300;400;700&family=Jura:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../../../public/assets/css/common.css">
 	<?= $this->getMeta(); ?>
+
+<!--  TODO pashtet: скорее всего вынести в body  -->
+    <script defer type="module" src="../../../public/assets/js/libs/index.js"></script>
 </head>
 <body>
-	<h1>Шаблон DEFAULT</h1>
 	<?= $content; ?>
 </body>
 </html>
