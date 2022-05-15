31a84b713e6ae5d161f083bb7290183553a83a33
Лебедева Наталья <NataRaposa@yandex.ru>
2022-05-15 17:23:03 +0200

    AppController.php: создала экземпляр класса модели

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -2,9 +2,14 @@
 
 namespace app\controllers;
 
+use app\models\AppModel;
 use vetgrad\base\Controller;
 
 class AppController extends Controller
 {
-
+	public function __construct($route)
+	{
+		parent::__construct($route);
+		new AppModel();
+	}
 }
