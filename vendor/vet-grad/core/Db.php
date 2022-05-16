41ecb371712189754d5187d3a848909724130e8f
Pashtet <web.shorohov@yandex.ru>
2022-05-16 17:17:54 +0200

    Временый костыль пока не подключился к БД

--- vendor/vet-grad/core/Db.php
+++ vendor/vet-grad/core/Db.php
@@ -10,14 +10,14 @@ class Db
 
 	protected function __construct()
 	{
-		$db = require_once CONF . '/config_db.php';
-		R::setup($db['dsn'], $db['user'], $db['pass']);
-		if (!R::testConnection()) {
-			throw new \Exception('Нет соединения с БД', 500);
-		}
-		R::freeze(true);
-		if (DEBUG) {
-			R::debug(true, 1);
-		}
+//		$db = require_once CONF . '/config_db.php';
+//		R::setup($db['dsn'], $db['user'], $db['pass']);
+//		if (!R::testConnection()) {
+//			throw new \Exception('Нет соединения с БД', 500);
+//		}
+//		R::freeze(true);
+//		if (DEBUG) {
+//			R::debug(true, 1);
+//		}
 	}
 }
