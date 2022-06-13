d08ce581885003411b8a0ee76badc3850ac5205e
Лебедева Наталья <NataRaposa@yandex.ru>
2022-06-13 22:56:58 +0200

    routes.php: добавила маршрут для мтраницы с карточками услуг

--- config/routes.php
+++ config/routes.php
@@ -2,6 +2,8 @@
 
 use vetgrad\Router;
 
+Router::add('^services/(?P<alias>[a-z0-9-]+)/?$', ['controller' => 'Services', 'action' => 'view']);
+
 //default routes
 Router::add('^admin$', ['controller' => 'Main', 'action' => 'index', 'prefix' => 'admin']);
 Router::add('^admin/?(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$', ['prefix' => 'admin']);
