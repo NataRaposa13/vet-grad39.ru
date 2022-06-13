250f992f8c5a8171605560a24e64354a568cf1a9
Pashtet <web.shorohov@yandex.ru>
2022-06-13 21:35:35 +0200

    Верстка страницы типов услуг

--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -5,51 +5,134 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../../../public/assets/css/pages/main.css">
+    <link rel="stylesheet" href="../../../public/assets/css/pages/specialization.css">
     <title>Главная</title>
 </head>
 <body>
-    <div class="main-page">
-        <section class="main-first-screen">
-            <div class="blob-wrap">
-                <div class="container container-relative">
-                    <div class="blue-blob_animated uk-animation-slide-bottom-medium"></div>
-                    <svg class="syrcle-grid uk-animation-slide-left-medium">
-                        <use xlink:href="./../../public/assets/img/icon/cyrcle-border.svg#s"/>
-                    </svg>
-                    <svg use="../../../public/assets/img/icon/cyrcle-border.svg#s" class="syrcle-grid uk-animation-slide-left-medium"/>
-                    <div class="syrcle-round uk-animation-slide-right-medium"></div>
-                </div>
-            </div>
-            <div class="container container-relative">
-                <img class="main-first-screen__image" src="../../../public/assets/img/content/first-screen-img.png" alt="" uk-parallax="start: 30vh; end: 30vh; y: 70; easing: 0;">
-                <div class="main-first-screen__inner">
-                    <h1 class="main-title">Большой спектр ветеренарных услуг</h1>
-                </div>
-            </div>
-        </section>
+<!--    <div class="main-page">-->
+<!--        <section class="main-first-screen">-->
+<!--            <div class="blob-wrap">-->
+<!--                <div class="container container-relative">-->
+<!--                    <div class="blue-blob_animated uk-animation-slide-bottom-medium"></div>-->
+<!--                    <svg class="syrcle-grid uk-animation-slide-left-medium">-->
+<!--                        <use xlink:href="./../../public/assets/img/icon/cyrcle-border.svg#s"/>-->
+<!--                    </svg>-->
+<!--                    <svg use="../../../public/assets/img/icon/cyrcle-border.svg#s" class="syrcle-grid uk-animation-slide-left-medium"/>-->
+<!--                    <div class="syrcle-round uk-animation-slide-right-medium"></div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--            <div class="container container-relative">-->
+<!--                <img class="main-first-screen__image" src="../../../public/assets/img/content/first-screen-img.png" alt="" uk-parallax="start: 30vh; end: 30vh; y: 70; easing: 0;">-->
+<!--                <div class="main-first-screen__inner">-->
+<!--                    <h1 class="main-title">Большой спектр ветеренарных услуг</h1>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </section>-->
+<!---->
+<!--        <section class="main-advantage">-->
+<!--            <div class="container container-relative">-->
+<!--                <div class="vet-card-list">-->
+<!--                    <div class="vet-card">-->
+<!--                        <img src="../../../public/assets/img/icon/main-pre-1.svg" class="vet-card__icon" alt=""/>-->
+<!--                        <div class="vet-card__title">Современное оборудование</div>-->
+<!--                        <div class="vet-card__text">В нашей клинике используется новейшее оборудование и только качетвенные медицинские препараты</div>-->
+<!--                    </div>-->
+<!--                    <div class="vet-card">-->
+<!--                        <img src="../../../public/assets/img/icon/main-pre-2.svg" class="vet-card__icon" alt=""/>-->
+<!--                        <div class="vet-card__title">Квалифицированные врачи</div>-->
+<!--                        <div class="vet-card__text">Все наши врачи имеют профессиональное высшее образование и большой стаж работы</div>-->
+<!--                    </div>-->
+<!--                    <div class="vet-card">-->
+<!--                        <img src="../../../public/assets/img/icon/main-pre-3.svg" class="vet-card__icon" alt=""/>-->
+<!--                        <div class="vet-card__title">Доступные цены</div>-->
+<!--                        <div class="vet-card__text">Мы стараемся делать наши достумными всем</div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </section>-->
+<!--    </div>-->
 
-        <section class="main-advantage">
-            <div class="container container-relative">
-                <div class="vet-card-list">
-                    <div class="vet-card">
-                        <img src="../../../public/assets/img/icon/main-pre-1.svg" class="vet-card__icon" alt=""/>
-                        <div class="vet-card__title">Современное оборудование</div>
-                        <div class="vet-card__text">В нашей клинике используется новейшее оборудование и только качетвенные медицинские препараты</div>
+
+    <div class="vet-service-page">
+        <div class="container">
+            <div class="vet-service-page__inner">
+                <h1 class="vet-service-page__title">
+                    Стоматология
+                </h1>
+
+                <div class="vet-service__list">
+                    <div class="vet-service__item">
+                        <div class="vet-service__name">
+                            Лечение кариеса
+                        </div>
+
+
+
+                        <div class="vet-service__item-footer">
+                            <button type="button" class="btn btn-primary btn-sm btn-vet-button">Записаться</button>
+
+                            <div class="vet-service__price">
+                                500 руб.
+                            </div>
+
+                        </div>
                     </div>
-                    <div class="vet-card">
-                        <img src="../../../public/assets/img/icon/main-pre-2.svg" class="vet-card__icon" alt=""/>
-                        <div class="vet-card__title">Квалифицированные врачи</div>
-                        <div class="vet-card__text">Все наши врачи имеют профессиональное высшее образование и большой стаж работы</div>
+
+                    <div class="vet-service__item">
+                        <div class="vet-service__name">
+                            Лечение кариеса
+                        </div>
+
+
+
+                        <div class="vet-service__item-footer">
+                            <button type="button" class="btn btn-primary btn-sm btn-vet-button">Записаться</button>
+
+                            <div class="vet-service__price">
+                                500 руб.
+                            </div>
+
+                        </div>
                     </div>
-                    <div class="vet-card">
-                        <img src="../../../public/assets/img/icon/main-pre-3.svg" class="vet-card__icon" alt=""/>
-                        <div class="vet-card__title">Доступные цены</div>
-                        <div class="vet-card__text">Мы стараемся делать наши достумными всем</div>
+
+                    <div class="vet-service__item">
+                        <div class="vet-service__name">
+                            Лечение кариеса
+                        </div>
+
+
+
+                        <div class="vet-service__item-footer">
+                            <button type="button" class="btn btn-primary btn-sm btn-vet-button">Записаться</button>
+
+                            <div class="vet-service__price">
+                                500 руб.
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="vet-service__item">
+                        <div class="vet-service__name">
+                            Лечение кариеса
+                        </div>
+
+
+
+                        <div class="vet-service__item-footer">
+                            <button type="button" class="btn btn-primary btn-sm btn-vet-button">Записаться</button>
+
+                            <div class="vet-service__price">
+                                500 руб.
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
-        </section>
+
+
+        </div>
     </div>
 
     <script type="module" src="../../../public/assets/js/index.js"></script>
