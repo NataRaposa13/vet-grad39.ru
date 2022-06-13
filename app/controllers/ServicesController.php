3706511d4683692a7acb13e29a851ec5623ba508
Лебедева Наталья <NataRaposa@yandex.ru>
2022-06-13 23:24:55 +0200

    перенесла шаблон карточек в view.php

--- app/controllers/ServicesController.php
+++ app/controllers/ServicesController.php
@@ -2,9 +2,15 @@
 
 namespace app\controllers;
 
+use RedBeanPHP\R as R;
+use vetgrad\App;
+
 class ServicesController extends AppController
 {
 	public function viewAction()
 	{
+		$serviceTypes = R::find( 'ServiceType');
+
+		$this->set(compact('serviceTypes'));
 	}
 }
