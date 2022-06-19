27def56f0cd01cd5bea449381eb3094c101525c1
Pashtet <web.shorohov@yandex.ru>
2022-06-19 20:21:36 +0200

    +

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -84,5 +84,6 @@
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script defer type="module" src="../../../public/assets/js/libs/index.js"></script>
 </body>
 </html>
